7d5189f8d685302a78d5fbf0c993ce8683cb0c55
sahandealmeidapey <39130343+sahandealmeidapey@users.noreply.github.com>
2019-06-07 17:07:16 -0400

    Configure KMH using component annotation

--- KafkaMicroservice/src/main/java/org/KafkaMicroservice/KafkaMessageHandler.java
+++ KafkaMicroservice/src/main/java/org/KafkaMicroservice/KafkaMessageHandler.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class KafkaMessageHandler {
     
     @Autowired
--- KafkaMicroservice/src/main/java/org/KafkaMicroservice/config/KafkaTopicConfig.java
+++ KafkaMicroservice/src/main/java/org/KafkaMicroservice/config/KafkaTopicConfig.java
@@ -33,8 +33,8 @@ public class KafkaTopicConfig {
          return new NewTopic(this.topicName, 1, (short) 1);
     }
     
-    @Bean
-    public KafkaMessageHandler getMessageInterpretter() {
-        return new KafkaMessageHandler();
-    }
+//    @Bean
+//    public KafkaMessageHandler getMessageInterpretter() {
+//        return new KafkaMessageHandler();
+//    }
 }
