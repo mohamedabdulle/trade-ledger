5b79b94900262d46c1046c64794a18c852701f1d
sahandealmeidapey <39130343+sahandealmeidapey@users.noreply.github.com>
2019-06-07 17:03:09 -0400

    Use application name instead of address to reference mongo service internally

--- KafkaMicroservice/src/main/java/org/KafkaMicroservice/KafkaMessageHandler.java
+++ KafkaMicroservice/src/main/java/org/KafkaMicroservice/KafkaMessageHandler.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,24 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 public class KafkaMessageHandler {
+    
     @Autowired
     private RestTemplate restTemplate;
 
     public void handleKafkaMessage(Object message) {
         String messageString = message.toString().trim().toLowerCase();
-        try {
             switch(messageString) {
                 case("get all"): {
+                    String address = "http://mongo-service/";
+                    String url = address + "subaccounting/document/";
                     @SuppressWarnings("unchecked")
-                    List<Object> documents = restTemplate.getForObject("http://localhost:8400/subaccounting/document/", List.class);
+                    List<Object> documents = restTemplate.getForObject(url, List.class);
                     System.out.println(documents);
                 }
             }            
-        }
-        catch(Exception error) {
-            System.out.println("couldn't handle kafka message");
-            error.printStackTrace();
-        }
+        
 
     }
 
