3349499525d3bcc0c5baf42bf664ab919276d386
sahandealmeidapey <salmeida@ifdsgroup.com>
2019-06-19 16:09:22 -0400

    started new sql module

---
+++ sql/src/main/java/org/sql/SqlApplication.java
@@ -0,0 +1,15 @@
+package org.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//@EnableEurekaClient     // Enable eureka client. It inherits from @EnableDiscoveryClient.
+public class SqlApplication { 
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlApplication.class, args);
+    }
+    
+}
