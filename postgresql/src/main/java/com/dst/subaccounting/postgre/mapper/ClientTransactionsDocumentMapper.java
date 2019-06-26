fa534630152142ccd9730cf2c3462272507c4544
sahandealmeidapey <salmeida@ifdsgroup.com>
2019-06-26 11:12:04 -0400

    fix mapper so it no longer fails

--- postgresql/src/main/java/com/dst/subaccounting/postgre/mapper/ClientTransactionsDocumentMapper.java
+++ postgresql/src/main/java/com/dst/subaccounting/postgre/mapper/ClientTransactionsDocumentMapper.java
@@ -5,20 +5,43 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.stream.Stream;
 
 public class ClientTransactionsDocumentMapper implements RowMapper<ClientTransactionsDocument> {
 
-    @Override
-    public ClientTransactionsDocument mapRow(ResultSet resultSet, int i) throws SQLException {
-        ClientTransactionsDocument doc = new ClientTransactionsDocument();
-        Stream.of(doc.getClass().getDeclaredFields()).forEach(f -> {
-            	try {
-					f.set(doc, resultSet.getObject(f.getName()));
-				} catch (IllegalArgumentException | IllegalAccessException | SQLException e) {
-					e.printStackTrace();
-				}        		
-        });
-        return doc;
-    }
+	@Override
+	public ClientTransactionsDocument mapRow(ResultSet resultSet, int i) throws SQLException {
+		ClientTransactionsDocument doc = new ClientTransactionsDocument();
+		 
+	    doc.setActualPostingDate(resultSet.getDate("actualPostingDate"));
+	    doc.setAgentForFirm(resultSet.getString("agentForFirm"));
+	    doc.setAnticipatedPostingDate(resultSet.getString("anticipatedPostingDate"));
+	    doc.setBin(resultSet.getString("bin"));
+	    doc.setBranch(resultSet.getString("branch"));
+	    doc.setClearingFirmNumber(resultSet.getInt("clearingFirmNumber"));
+	    doc.setCountryCode(resultSet.getString("countryCode"));
+	    doc.setCurrencyIndicator(resultSet.getString("currencyIndicator"));
+	    doc.setDealerFirmNumber(resultSet.getInt("dealerFirmNumber"));
+	    doc.setEffectOnBalance(resultSet.getString("effectOnBalance"));
+	    doc.setFund_id(resultSet.getString("fund_id")); // Reference to _id field within security master
+	    doc.setMoneyAmount(resultSet.getString("moneyAmount"));
+	    doc.setOmnibusTransaction_Id(resultSet.getString("omnibusTransaction_Id")); // Reference to "_id" from omnibus
+	    doc.setPrice(resultSet.getDouble("price"));
+	    doc.setRepresentativeName(resultSet.getString("representativeName"));
+	    doc.setRepresentativeNumber(resultSet.getInt("representativeNumber"));
+	    doc.setSecurityId(resultSet.getString("securityId"));
+	    doc.setSequenceId(resultSet.getString("sequenceId"));
+	    doc.setShareQuantity(resultSet.getString("shareQuantity"));
+	    doc.setStateOfSale(resultSet.getString("stateOfSale"));
+	    doc.setSubmissionDate(resultSet.getString("submissionDate"));
+	    doc.setSymbol(resultSet.getString("symbol"));
+	    doc.setTradeDate(resultSet.getString("tradeDate")); // Would be date field
+	    doc.setTransactionCategory(resultSet.getString("transactionCategory"));
+	    doc.setTransactionSource(resultSet.getString("transactionSource"));
+	    doc.setTransactionStatus(resultSet.getString("transactionStatus"));
+	    doc.setTransactionSubType(resultSet.getString("transactionSubType"));
+	    doc.setTransactionType(resultSet.getString("transactionType"));
+	    doc.setTransactionUserId(resultSet.getString("transactionUserId"));
+		
+		return doc;
+	}
 }
